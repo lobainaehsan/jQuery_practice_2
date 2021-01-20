d35e8f440a2c660dd86ff52325d83a4b9494bb34
lobainaehsan <72964587+lobainaehsan@users.noreply.github.com>
2021-01-20 21:06:51 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+ // your-script.js
+              new Klouds({
+                selector: '#myCanvas',
+                layerCount: 5,
+                speed: 1,
+                cloudColor1: '#1ab3cc',
+                cloudColor2: '#ffffff',
+                bgColor: '#06680'
+              })
